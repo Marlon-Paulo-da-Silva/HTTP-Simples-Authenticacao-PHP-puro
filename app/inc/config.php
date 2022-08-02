1ee64b3ae7d8ce87da994bd8e7dfa9c002f22d0f
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-08-02 08:35:55 -0300

    Fazendo a verificação do usuário e senha dentro da api password_hash com vários usuários

--- app/inc/config.php
+++ app/inc/config.php
@@ -1,5 +1,16 @@
 <?php
 
 define("API_BASE_ENDPOINT", "http://localhost/HTTP-Simples-Authenticacao-PHP-puro/api/");
-define("API_USER", "jn76mj4l6ken5tvdzfv5");
-define("API_PASS", "aul9fvzahwiarw89zmho");
+
+// // joao
+// define("API_USER", "jn76mj4l6ken5tvdzfv5");
+// define("API_PASS", "aul9fvzahwiarw89zmho");
+
+// //ana
+// define("API_USER", "inki9q9y8kzcp9ccitwdcbdrmuy8n3");
+// define("API_PASS", "64xuenk55eidafz9mrdgwyxaigqb2j");
+
+//roberto
+define("API_USER", "4qw44qyidb4b8ch6ab6uvnvwewgy39");
+define("API_PASS", "wk88tdaix6priw5j7fuxvwkejnby5r");
+
